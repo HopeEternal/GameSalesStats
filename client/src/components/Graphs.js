d97a30e24f83517c22cd0fa7b6c41c58e7dc897c
HopeEternal <risalee316@gmail.com>
2020-02-15 18:36:07 -0500

    App responsivity adjusted - table component still needs to be responsive

--- client/src/components/Graphs.js
+++ client/src/components/Graphs.js
@@ -8,10 +8,10 @@ export default function Graphs() {
     return (
       <Container maxWidth="lg">
         <Grid container spacing={6}>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={12} md={6}>
             <GenreGraph />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={12} md={6}>
            <PopularityGraph />
           </Grid>
         </Grid>
--- client/src/components/Introduction.js
+++ client/src/components/Introduction.js
@@ -7,14 +7,14 @@ export default function Introduction() {
     return (
       <Container maxWidth="lg">
         <Grid container spacing={6}>
-          <Grid item xs={8}>
+          <Grid item xs={12} sm={8} md={8}>
             <h1 color="primary">What games sold the best worldwide?</h1>
             <p>Have you ever wondered what the top selling games were? Thanks to Gregory Smith, you can find out! Using a scrape of <a href="vgchartz.com" target="_blank" rel="noopener noreferrer">vgchartz.com</a>, you can check out this selection of data on titles with sales greater than 100,000 copies.</p>
             <p>This dataset includes console and PC games and games from companies as big as Nintendo and as small as Yeti.</p>
             <p>Download the original data set yourself at <a href="https://www.kaggle.com/gregorut/videogamesales" target="_blank" rel="noopener noreferrer">Kaggle.com</a>.</p>
             <p>Thanks to <a href="https://www.vecteezy.com/free-vector/mountain" target="_blank" rel="noopener noreferrer">Mountain Vectors by Vecteezy</a> for providing this beautiful image.</p>
           </Grid>
-          <Grid item xs>
+          <Grid item xs xs={12} sm={4} md={4}>
             <img src={GamerImage} width="100%" alt="A family celebrating over video games" />
           </Grid>
         </Grid>
